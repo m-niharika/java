433a7e70db0e96dfb269389bdc97021464cd6fda
m-niharika <niharikamaheshwari7@gmail.com>
2019-12-12 22:43:52 +0530

    changes reverted

--- StringPrograms/src/GenericClass.java
+++ StringPrograms/src/GenericClass.java
@@ -3,11 +3,15 @@ import java.util.List;
 public class GenericClass {
 	
 	public void listAdd(List<Object> list){
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 		System.out.println("accepted Generics first");
 =======
 		System.out.println("accepted");
 >>>>>>> parent of 013ee59... First commit in develop
+=======
+		System.out.println("changes before revert commit");
+>>>>>>> Stashed changes
 	}
 
 }
