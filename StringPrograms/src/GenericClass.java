013ee591299f80204c38d516d48c0e8e58582f3c
m-niharika <niharikamaheshwari7@gmail.com>
2019-12-12 22:33:16 +0530

    First commit in develop

--- StringPrograms/src/GenericClass.java
+++ StringPrograms/src/GenericClass.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class GenericClass {
 	
 	public void listAdd(List<Object> list){
-		System.out.println("accepted");
+		System.out.println("accepted Generics");
 	}
 
 }
