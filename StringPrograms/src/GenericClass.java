58a538f965786c482b2e1af8e44aeee09044ceca
m-niharika <niharikamaheshwari7@gmail.com>
2019-12-12 22:42:08 +0530

    changes reverted

--- StringPrograms/src/GenericClass.java
+++ StringPrograms/src/GenericClass.java
@@ -3,7 +3,11 @@ import java.util.List;
 public class GenericClass {
 	
 	public void listAdd(List<Object> list){
+<<<<<<< HEAD
 		System.out.println("accepted Generics first");
+=======
+		System.out.println("accepted");
+>>>>>>> parent of 013ee59... First commit in develop
 	}
 
 }
